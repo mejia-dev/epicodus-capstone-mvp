636df7e764bac7fdc2f544c8ccbaa2b75de9836c
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-02 11:24:55 -0700

    Add play button functionality and fix bug where audio buffer was not created correctly (line 22)

--- game_and_collision/demov4.js
+++ game_and_collision/demov4.js
@@ -2,6 +2,9 @@
   // get canvas and set it to 2d
   // get the audio upload spot and set the event handler to change
 
+let globalAudioBuffer;
+let globalAudioContext;
+let globalAudioHTMLElement;
 let globalCanvas;
 let globalCtx;
 
@@ -16,20 +19,45 @@ function handleAudioUpload(event) {
   const file = event.target.files[0], fileUrl = blob.createObjectURL(file);
   document.getElementById("audioSource").src = fileUrl;
   const fileReader = new FileReader();
-  fileReader.onload = () => {
+  fileReader.onload = function () {
     const bufferedAudioArray = this.result;
-    // do something with the buffered audio array here
+    // do something with buffered audio array here
     initializeAudioTrack(bufferedAudioArray);
   };
   fileReader.readAsArrayBuffer(file);
 }
 
 
+// attach audio context to the uploaded track
 function initializeAudioTrack(bufferedAudioArray) {
-
+  globalAudioContext = new (window.AudioContext || window.webkitAudioContext)();
+  globalAudioHTMLElement = document.getElementById("audioSource");
+  const track = globalAudioContext.createMediaElementSource(globalAudioHTMLElement);
+  track.connect(globalAudioContext.destination);
+  globalAudioContext.decodeAudioData(bufferedAudioArray, (buffer) => {
+    globalAudioBuffer = buffer;
+    // proceed to next steps after audio has decoded here
+    initializeAudioControls();
+  })
 }
 
-
+function initializeAudioControls() {
+  const playButton = document.getElementById("playButton");
+  playButton.addEventListener("click", () => {
+    if (globalAudioContext.state === "suspended") {
+      globalAudioContext.resume();
+    }
+    if (playButton.dataset.playing === "false") {
+      globalAudioHTMLElement.play();
+      playButton.dataset.playing = "true";
+    } else if (playButton.dataset.playing === "true") {
+      globalAudioHTMLElement.pause();
+      playButton.dataset.playing = "false";
+    }
+  },
+    false,
+  );
+}
 
 
 // Create Level from Audio
